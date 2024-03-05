99616435dc02649e43afd2d101564c3c05a6f899
hrayr <i@hrayr.am>
2024-03-05 23:33:34 +0400

    fix: slugify the slug

--- blog/admin.py
+++ blog/admin.py
@@ -21,7 +21,6 @@ class ArticleAdmin(admin.ModelAdmin):
     date_hierarchy = 'published_at'
     form = ArticleForm
     inlines = [ArticleContentInline]
-    prepopulated_fields = {'slug': ('slug',), }
 
     def save_model(self, request, obj, form, change):
         obj.creator = request.user
